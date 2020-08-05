efe9b2c2f5bb62509effce515a52ed5f168114a5
chuck francis <chuck_francis10@yahoo.com>
2020-08-05 16:02:45 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BestAdderEver08052020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int tapes, cds, collection;
+
+            double number;
+
+            tapes = 56;
+            cds = 126;
+
+            collection = cds + tapes;
+
+            cds--;
+            tapes--;
+            Console.WriteLine(" tapes + cds ");
+            Console.WriteLine("I have tapes + cds in my collection");
+            Console.WriteLine(collection);
+        }
+    }
+}
